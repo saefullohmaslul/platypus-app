f4077cca5ce1e0835bfa0295d5cfce269296d854
Akhmad Maknur <akhmad.maknur@gmail.com>
2022-07-27 22:02:15 +0800

    feat: add model cards and points

---
+++ db/models/cards.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+const sequelize = require('./sequelize');
+
+class Cards extends Sequelize.model {}
+
+Cards.init(
+  {
+    id: {
+      type: Sequelize.DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
+    },
+    user_id: {
+      type: Sequelize.DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id'
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    },
+    type: {
+      type: Sequelize.DataTypes.STRING
+    },
+    status: {
+      type: Sequelize.DataTypes.ENUM,
+      values: ['ACTIVE', 'NON_ACTIVE']
+    }
+  },
+  {
+    sequelize: sequelize,
+    timestamps: true,
+    underscored: true,
+    paranoid: true,
+    freezeTableName: true,
+    tableName: 'cards'
+  }
+);
+
+module.exports = Cards;
--- db/models/index.js
+++ db/models/index.js
@@ -1,19 +1,41 @@
-const Roles = require('./roles')
-const Users = require('./users')
-const sequelize = require('./sequelize')
+const Roles = require('./roles');
+const Users = require('./users');
+const sequelize = require('./sequelize');
+const Cards = require('./cards');
+const Points = require('./points');
 
 Roles.hasMany(Users, {
   as: 'users',
-  foreignKey: 'role_id',
-})
+  foreignKey: 'role_id'
+});
 
 Users.belongsTo(Roles, {
   as: 'role',
-  foreignKey: 'role_id',
-})
+  foreignKey: 'role_id'
+});
+
+Cards.belongsTo(Users, {
+  as: 'users',
+  foreignKey: 'user_id'
+});
+
+Points.belongsTo(Cards, {
+  as: 'cards',
+  foreignKey: 'card_id'
+});
+
+Cards.hasOne(Points, {
+  as: 'points',
+  foreignKey: 'card_id'
+});
+
+Users.hasOne(Cards, {
+  as: 'card_user',
+  foreignKey: 'user_id'
+});
 
 module.exports = {
   sequelize,
   Users,
-  Roles,
-}
+  Roles
+};
