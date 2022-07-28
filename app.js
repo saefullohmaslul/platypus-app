58f313b6ecfaccd6579a2857f2ead8e526fff95b
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2022-07-28 20:12:47 +0700

    fix: remove sync force

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const userRouter = require('./routes/users')
-const {sequelize} = require('./db/models')
 
 const app = express()
-
-sequelize.sync({force: true}).catch(err => console.log(err))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
