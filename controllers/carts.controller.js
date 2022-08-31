20962198d8e80163fc8dd3ffb957e50e2a1dac5d
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2022-08-31 21:37:50 +0700

    feat: add get cart

--- controllers/carts.controller.js
+++ controllers/carts.controller.js
@@ -1,4 +1,5 @@
 const Carts = require("../db/schemas/cart.schema")
+const NewError = require("../helpers/error-stack.helper")
 
 exports.createCart = async (req, res, next) => {
     try {
@@ -6,6 +7,24 @@ exports.createCart = async (req, res, next) => {
 
         return res.status(200).json({
             message: 'success create cart',
+            data: cart
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.getCart = async (req, res, next) => {
+    try {
+        const cartId = req.params.id
+        const cart = await Carts.findOne({ _id: cartId })
+        
+        if (!cart) {
+            throw new NewError(404, 'cart not found')
+        }
+
+        return res.status(200).json({
+            message: 'success get cart',
             data: cart
         })
     } catch (error) {
--- routes/carts.router.js
+++ routes/carts.router.js
@@ -1,7 +1,8 @@
-const { createCart } = require('../controllers/carts.controller')
+const { createCart, getCart } = require('../controllers/carts.controller')
 
 const router = require('express').Router()
 
 router.post('/', createCart)
+router.get('/:id', getCart)
 
 module.exports = router
