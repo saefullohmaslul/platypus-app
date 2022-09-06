585fe0a48d8ab81e6185f2fe23860ec57f9f529c
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2022-09-06 21:48:26 +0700

    build: add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    apps: [
+      {
+        name: 'appa',
+        script: 'index.js',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  
+    deploy: {
+      production: {
+        user: 'node',
+        host: '128.199.107.27',
+        ref: 'origin/master',
+        repo: 'git@github.com:saefullohmaslul/platypus-app.git',
+        path: '~/platypus-app',
+        'post-deploy':
+          'npm install && pm2 reload ecosystem.config.js --env production',
+      },
+    },
+  };
